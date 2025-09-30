8db39e964c994f51ef87f0720957cbafcf34f275
Cong <bcong@crai.com>
2025-09-30 12:18:41 -0500

    feat: first draft of extracting IPUMS

--- extract_ipums.py
+++ extract_ipums.py
@@ -1,2 +1,24 @@
-import ipumspy
-import dotenv
+from ipumspy import IpumsApiClient, AggregateDataExtract, NhgisDataset
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+
+load_dotenv()
+
+IPUMS_API_KEY = os.environ.get("API_KEY")
+ipums = IpumsApiClient(IPUMS_API_KEY)
+
+# Create an extract
+extract = AggregateDataExtract(
+   collection="nhgis",
+   description="2000 Census Race",
+   datasets=[
+      NhgisDataset(name="2000_SF2", data_tables=['NPCT001A'], geog_levels=["tract"])
+   ]
+)
+
+ipums.submit_extract(extract)
+ipums.wait_for_extract(extract)
+
+DOWNLOAD_DIR = Path(Path.cwd() / "data/")
+ipums.download_extract(extract, download_dir=DOWNLOAD_DIR)
